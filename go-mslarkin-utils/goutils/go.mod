00a5d603cee444d9ff586f400517cdc5c82b7cfc
Matt Larkin <matthewlarkin@google.com>
2024-06-04 10:10:25 -0700

    .

---
+++ go-mslarkin-utils/goutils/go.mod
@@ -0,0 +1,3 @@
+module github.com/mlarkin00/mslarkin/go-mslarkin-utils/goutils
+
+go 1.21.0
---
+++ go-mslarkin-utils/goutils/goutils.go
@@ -0,0 +1,33 @@
+package goutils
+
+import (
+	// "context"
+	// "fmt"
+	// "io"
+	"net/http"
+	"net/url"
+	"os"
+	// "regexp"
+	// "time"
+	// "log"
+	// "strconv"
+)
+
+func GetParam(r *http.Request, p string, d string) string {
+
+	params, _ := url.ParseQuery(r.URL.RawQuery)
+	value := params.Get(p)
+	if len(value) > 0 {
+		return value
+	} else {
+		return d
+	}
+}
+
+func GetEnv(key, d string) string {
+    value := os.Getenv(key)
+    if len(value) == 0 {
+        return d
+    }
+    return value
+}
