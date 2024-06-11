45117f6121ee7ac5e9c2a037349f4dca25950ddb
Matt Larkin <matthewlarkin@google.com>
2024-06-11 19:42:24 +0000

    .

--- go-mslarkin-utils/localTesting/localTesting.go
+++ go-mslarkin-utils/localTesting/localTesting.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"context"
+	// "context"
 	"fmt"
 	"os"
 	// "regexp"
 	// "math"
 	"os/signal"
-	"runtime"
+	// "runtime"
 	"syscall"
-	"time"
+	// "time"
 	gcputils "github.com/mlarkin00/mslarkin/go-mslarkin-utils/gcputils"
 	// goutils "github.com/mlarkin00/mslarkin/go-mslarkin-utils/goutils"
 	// loadgen "github.com/mlarkin00/mslarkin/go-mslarkin-utils/loadgen"
@@ -41,97 +41,5 @@ func main() {
 	fmt.Println(gcputils.GetInstanceCount("go-worker", "mslarkin-ext", "us-central1"))
 	// fmt.Println(gcputils.GetRunService("go-worker", "mslarkin-ext", "us-central1"))
 
-	loadCtx, loadCtxCancel := context.WithCancel(context.Background())
-	defer loadCtxCancel()
-
-	numCpus := runtime.NumCPU()
-	fmt.Println(numCpus, "CPUs available")
-
-	targetCpuPct := float64(50) //strconv.Atoi(goutils.GetParam(r, "targetCpuPct", "40"))
-	durationS := 10             //strconv.Atoi(goutils.GetParam(r, "durationS", "10"))
-
-	tgtPct := targetCpuPct / 100
-	targetCpus = 1 //int(math.Round(float64(numCpus) * tgtPct))
-	fmt.Println("Total CPUs:", numCpus, " Target Pct:", targetCpuPct, ", %:", tgtPct, " Target CPUs:", targetCpus, " Duration:", durationS)
-
-	go func() {
-		defer loadCtxCancel()
-		for {
-			fmt.Println("Starting loop...")
-			// fmt.Printf("Context: %v | updateLoad: %v | CPUs: %v", loadCtx, updateLoad, targetCpus)
-			// go cpuLoadGen(loadCtx, updateLoad, targetCpus)
-			varTest(loadCtx, &updateLoad, targetCpus)
-
-			fmt.Println("Loading...")
-			time.Sleep(time.Duration(durationS) * time.Second)
-			fmt.Println("Updating...")
-			targetCpus++ 
-			updateLoad = true
-			// fmt.Println("Second Waiting...")
-			// fmt.Printf("Context: %v | updateLoad: %v | CPUs: %v", loadCtx, updateLoad, targetCpus)
-			// time.Sleep(time.Duration(durationS) * time.Second)
-		}
-	}()
-
-	// for {
-	// 	select {
-	// 	case <- loadCtx.Done():
-	// 		fmt.Println("Exiting due to Context")
-	// 		return
-	// 	case sig := <-signalChan:
-	// 		fmt.Printf("%s signal caught", sig)
-	// 		return
-	// 	}
-	// }
-
-	sig := <-signalChan
-	fmt.Printf("%s signal caught", sig)
-	// close(done)
-
-}
-
-func varTest(ctx context.Context, updateLoad *bool, targetCpus int) {
-	fmt.Println("Testing CPUs:", targetCpus)
-	for i := 0; i < targetCpus; i++ {
-		go func() {
-			for {
-				if *updateLoad {
-					fmt.Println("Exiting: Update")
-					return
-				} else {
-					select {
-					case <-ctx.Done():
-						// exitReason = "context.Done()"
-						fmt.Println("Exiting: Context")
-						return
-					default:
-					}
-				}
-			}
-		}()
-	}
-	// fmt.Println("Leaving test function")
-}
-
-func cpuLoadGen(ctx context.Context, updateLoad chan bool, targetCpus int) {
-	fmt.Println("Loading", targetCpus, "CPUs")
-	exitReason := "default"
-	for i := 0; i < targetCpus; i++ {
-		go func() {
-			for {
-				select {
-				case <-ctx.Done():
-					exitReason = "context.Done()"
-					fmt.Println("Exiting: Context")
-					return
-				case <-updateLoad:
-					exitReason = "updateLoad"
-					fmt.Println("Exiting: Update")
-					return
-				default:
-				}
-			}
-		}()
-	}
-	fmt.Println(exitReason)
+	
 }
