a327133f38df4dd8bb970c64fd32914b8f550813
Matt Larkin <matthewlarkin@google.com>
2024-06-13 12:32:20 -0700

    Adding roundFloat

--- go-mslarkin-utils/goutils/goutils.go
+++ go-mslarkin-utils/goutils/goutils.go
@@ -4,6 +4,7 @@ import (
 	// "context"
 	// "fmt"
 	// "io"
+	"math"
 	"net/http"
 	"net/url"
 	"os"
@@ -25,9 +26,14 @@ func GetParam(r *http.Request, p string, d string) string {
 }
 
 func GetEnv(key, d string) string {
-    value := os.Getenv(key)
-    if len(value) == 0 {
-        return d
-    }
-    return value
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return d
+	}
+	return value
+}
+
+func roundFloat(val float64, precision uint) float64 {
+	ratio := math.Pow(10, float64(precision))
+	return math.Round(val*ratio) / ratio
 }
