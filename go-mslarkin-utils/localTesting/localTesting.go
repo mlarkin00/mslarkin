9462289a4b4552946d67e5986c5f10025c51ea06
Matt Larkin <matthewlarkin@google.com>
2024-06-26 08:42:45 -0700

    .

--- go-mslarkin-utils/localTesting/localTesting.go
+++ go-mslarkin-utils/localTesting/localTesting.go
@@ -19,8 +19,8 @@ import (
 	// goutils "github.com/mlarkin00/mslarkin/go-mslarkin-utils/goutils"
 	// loadgen "github.com/mlarkin00/mslarkin/go-mslarkin-utils/loadgen"
 	// pubsub "cloud.google.com/go/pubsub"
-	run "cloud.google.com/go/run/apiv2"
-	runpb "cloud.google.com/go/run/apiv2/runpb"
+	// run "cloud.google.com/go/run/apiv2"
+	// runpb "cloud.google.com/go/run/apiv2/runpb"
 	monitoring "cloud.google.com/go/monitoring/apiv3/v2"
 	monitoringpb "cloud.google.com/go/monitoring/apiv3/v2/monitoringpb"
 	"google.golang.org/api/iterator"
@@ -40,6 +40,7 @@ type TokenResponse struct {
 var topicId string = "pull-test"                 //"my-topic"
 var subscriptionId string = "pull-queue-testing" //"my-pull-subscription"
 var projectId string = "mslarkin-ext"            //"my-project-id"
+var projectNum string = "79309377625"
 var subscriberServiceName string = "go-pubsub-subscriber"
 var subscriberRegion string = "us-central1"
 
@@ -48,52 +49,21 @@ func main() {
 	// SIGTERM handles Cloud Run termination signal.
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	go func() {
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
 		for {
-			// publishRate := roundFloat(getPublishRate(ctx, topicId, projectId))
-			// ackRate := roundFloat(getAckRate(ctx, subscriptionId, projectId))
-			// fmt.Printf("Publish Rate: %v, Ack Rate: %v\n", publishRate, ackRate)
-			subscriberService, _ := getRunService(ctx, subscriberServiceName, projectId, subscriberRegion)
-			configuredInstances := subscriberService.Scaling.MinInstanceCount
-			currentInstances := getInstanceCount(ctx, subscriberServiceName, projectId, subscriberRegion)
-			fmt.Printf("Service: %s, Instances: Configured: %v | Current: %v\n",
-				subscriberServiceName, configuredInstances, currentInstances)
+			publishRate := roundFloat(getPublishRate(ctx, topicId, projectId))
+			publishRateQ := roundFloat(getPublishRateQ(ctx, topicId, projectId))
+			publishRequestRate := roundFloat(getPublishRequestRate(ctx, topicId, projectId))
+			publishRequestRateQ := roundFloat(getPublishRequestRateQ(ctx, topicId, projectId))
+			ackRate := roundFloat(getAckRate(ctx, subscriptionId, projectId))
+			fmt.Printf("Publish Rate: %v (Query: %v), Publish Request Rate: %v (Query: %v), Ack Rate: %v\n",
+				publishRate, publishRateQ, publishRequestRate, publishRequestRateQ, ackRate)
 			time.Sleep(10 * time.Second)
 		}
 	}()
-
-	// var token TokenResponse
-
-	// tokenResp := `{
-	// 	"access_token":"ya29.AHES6ZRN3-HlhAPya30GnW_bHSb_QtAS08i85nHq39HE3C2LTrCARA",
-	// 	"expires_in":3599,
-	// 	"token_type":"Bearer"
-	// 	}`
-
-	// json.Unmarshal([]byte(tokenResp), &token)
-
-	// fmt.Println(tokenResp)
-	// fmt.Println(token)
-	// fmt.Println(token.AccessToken)
-
-	// projectId := "mslarkin-ext"
-	// topicId := "pull-test"
-	// // subId := "pull-queue-testing"
-
-	// go func() {
-	// 	for {
-	// 		getPubsubPublishRate(topicId, projectId)
-	// 		time.Sleep(10 * time.Second)
-	// 	}
-	// }()
-
-	// runService, err = gcputils.GetRunService("go-worker", "mslarkin-ext", "us-central1")
-	// udt := gcputils.GetLastUpdateTs("pubsub-pull-subscriber", "mslarkin-ext", "us-central1")
-	// fmt.Println(udt)
-	// fmt.Println(gcputils.GetInstanceCount("go-worker", "mslarkin-ext", "us-central1"))
-	// fmt.Println(gcputils.GetRunService("go-worker", "mslarkin-ext", "us-central1"))
 
 	// Receive output from signalChan.
 	sig := <-signalChan
@@ -103,25 +73,299 @@ func main() {
 
 func roundFloat(num float64) float64 { return float64(math.Round(num*100) / 100) }
 
-func getInstanceCount(ctx context.Context, service string, projectId string, region string) int {
-	monitoringMetric := "run.googleapis.com/container/instance_count"
+func getPublishRateQ(ctx context.Context, topicId string, projectId string) float64 {
+	monitoringMetric := "pubsub.googleapis.com/topic/message_sizes"
+
+	groupBy := []string{"resource.topic_id"}
+	metricFilter := fmt.Sprintf("resource.project_id == '%s'"+
+		" && resource.topic_id == '%s'",
+		projectId, topicId)
+
+	mqlQuery := fmt.Sprintf("fetch pubsub_topic"+
+		"| metric '%s'"+
+		"| filter %s"+
+		"| align delta(1m)"+
+		"| every 1m"+
+		"| group_by %s, [value_message_sizes_sum: count(value.message_sizes)]"+
+		"| align rate(1m)"+
+		"| within 1m, -240s",
+		monitoringMetric, metricFilter, groupBy)
+
+	// queryStart := time.Now()
+	// fmt.Printf("Query Start: %v\n", queryStart)
+	metricData := getMetricQuery(ctx, projectId, mqlQuery)
+	// fmt.Println("Metric Length:", len(metricData))
+
+	var metricValues []float64
+	for i := range metricData {
+		// endTime := metricData[i].TimeInterval.EndTime.AsTime()
+		// fmt.Printf("Metric End: %v, Value: %v\n", queryStart.Sub(endTime), metricData[i].GetValues())
+		metricValues = append(metricValues, metricData[i].GetValues()[0].GetDoubleValue())
+	}
+	// fmt.Printf("Metric Values: %v\n", metricValues)
+	//Return the latest rate datapoint
+	return metricValues[0]
+}
+
+func getPublishRate(ctx context.Context, topicId string, projectId string) float64 {
+	monitoringMetric := "pubsub.googleapis.com/topic/message_sizes"
 	aggregationSeconds := 60
 	intervalSeconds := 240
-	groupBy := []string{"resource.labels.service_name"}
+	groupBy := []string{"resource.labels.topic_id"}
 	metricFilter := fmt.Sprintf("metric.type=\"%s\""+
-		" AND resource.labels.service_name =\"%s\""+
-		" AND resource.labels.location =\"%s\"",
-		monitoringMetric, service, region)
+		" AND resource.labels.topic_id =\"%s\"",
+		monitoringMetric, topicId)
 
-	metricData := getMetricMean(ctx,
+	metricData := getMetricDelta(ctx,
 		metricFilter,
 		intervalSeconds,
 		aggregationSeconds,
 		groupBy,
 		projectId)
 
-	fmt.Println(metricData)
-	return int(metricData[0].GetValue().GetDoubleValue())
+	// for i := range metricData {
+	// 	fmt.Printf("Point: %v\n", metricData[i])
+	// 	// fmt.Printf("Value: %v\n", metricData[i].Value.Value)
+	// 	fmt.Printf("DistributionValue: %v\n", metricData[i].Value.GetDistributionValue())
+	// 	fmt.Printf("Distribution Count: %v\n", metricData[i].Value.GetDistributionValue().Count)
+	// }
+	messagesPerMinute := float64(metricData[0].Value.GetDistributionValue().Count)
+
+	//Return the latest rate datapoint (converted to seconds)
+	return messagesPerMinute/60
+}
+
+func getAckRate(ctx context.Context, subscriptionId string, projectId string) float64 {
+	monitoringMetric := "pubsub.googleapis.com/subscription/ack_message_count"
+	aggregationSeconds := 60
+	intervalSeconds := 240
+	groupBy := []string{"resource.labels.topic_id"}
+	metricFilter := fmt.Sprintf("metric.type=\"%s\""+
+		" AND resource.labels.subscription_id =\"%s\"",
+		monitoringMetric, subscriptionId)
+
+	metricData := getMetricRate(ctx,
+		metricFilter,
+		intervalSeconds,
+		aggregationSeconds,
+		groupBy,
+		projectId)
+
+	//Return the latest rate datapoint
+	return float64(metricData[0].GetValue().GetDoubleValue())
+}
+
+func getPublishRequestRate(ctx context.Context, topicId string, projectId string) float64 {
+	monitoringMetric := "pubsub.googleapis.com/topic/send_request_count"
+	aggregationSeconds := 60
+	intervalSeconds := 120
+	groupBy := []string{"resource.labels.topic_id"}
+	metricFilter := fmt.Sprintf("metric.type=\"%s\""+
+		" AND resource.labels.topic_id =\"%s\"",
+		monitoringMetric, topicId)
+
+	metricData := getMetricRate(ctx,
+		metricFilter,
+		intervalSeconds,
+		aggregationSeconds,
+		groupBy,
+		projectId)
+
+	//Return the latest rate datapoint
+	return float64(metricData[0].GetValue().GetDoubleValue())
+}
+
+func getPublishRequestRateQ(ctx context.Context, topicId string, projectId string) float64 {
+	monitoringMetric := "pubsub.googleapis.com/topic/send_request_count"
+
+	groupBy := []string{"resource.topic_id"}
+	metricFilter := fmt.Sprintf("resource.project_id == '%s'"+
+		" && resource.topic_id == '%s'",
+		projectId, topicId)
+
+	mqlQuery := fmt.Sprintf("fetch pubsub_topic"+
+		"| metric '%s'"+
+		"| filter %s"+
+		"| align rate(1m)"+
+		"| every 1m"+
+		"| group_by %s, [value_send_request_count_aggregate: aggregate(value.send_request_count)]"+
+		"| within 1m, -120s",
+		monitoringMetric, metricFilter, groupBy)
+
+	// fetch pubsub_topic
+	// | metric 'pubsub.googleapis.com/topic/send_request_count'
+	// | filter (resource.topic_id == 'pull-test')
+	// | align rate(1m)
+	// | every 1m
+	// | group_by [],
+	//     [value_send_request_count_aggregate: aggregate(value.send_request_count)]
+
+	metricData := getMetricQuery(ctx, projectId, mqlQuery)
+
+	var metricValues []float64
+	for i := range metricData {
+		metricValues = append(metricValues, metricData[i].GetValues()[0].GetDoubleValue())
+	}
+
+	//Return the latest rate datapoint
+	return metricValues[0]
+}
+
+func getMetricQuery(ctx context.Context, projectId string, mqlQuery string) []*monitoringpb.TimeSeriesData_PointData {
+	c, err := monitoring.NewQueryClient(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
+	req := &monitoringpb.QueryTimeSeriesRequest{
+		Name:  "projects/" + projectId,
+		Query: mqlQuery,
+	}
+	it := c.QueryTimeSeries(ctx, req)
+
+	var point []*monitoringpb.TimeSeriesData_PointData
+	for {
+		resp, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		// fmt.Printf("TimeSeriesData: %v\n", resp)
+		point = resp.GetPointData()
+		// pt := resp.GetPointData()
+		// for i := range pt {
+		// 	fmt.Printf("End: %v, Value: %v\n", pt[i].TimeInterval.EndTime, pt[i].GetValues())
+		// }
+	}
+	return point
+}
+
+func getMetricDelta(ctx context.Context,
+	resourceFilter string,
+	intervalSeconds int,
+	aggregationSeconds int,
+	groupBy []string,
+	projectId string) []monitoringpb.Point {
+	client, err := monitoring.NewMetricClient(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	//Configure metric aggregation
+	aggregationStruct := &monitoringpb.Aggregation{
+		// CrossSeriesReducer: monitoringpb.Aggregation_REDUCE_NONE,
+		PerSeriesAligner:   monitoringpb.Aggregation_ALIGN_DELTA,
+		GroupByFields:      groupBy,
+		AlignmentPeriod: &durationpb.Duration{
+			Seconds: int64(aggregationSeconds),
+		},
+	}
+
+	//Configure metric interval
+	startTime := time.Now().UTC().Add(time.Second * -time.Duration(intervalSeconds+aggregationSeconds))
+	endTime := time.Now().UTC().Add(time.Second * -time.Duration(intervalSeconds))
+
+	interval := &monitoringpb.TimeInterval{
+		StartTime: &timestamppb.Timestamp{
+			Seconds: startTime.Unix(),
+		},
+		EndTime: &timestamppb.Timestamp{
+			Seconds: endTime.Unix(),
+		},
+	}
+
+	req := &monitoringpb.ListTimeSeriesRequest{
+		Name:        "projects/" + projectId,
+		Filter:      resourceFilter,
+		Interval:    interval,
+		Aggregation: aggregationStruct,
+		// SecondaryAggregation: secondaryAggregationStruct,
+	}
+
+	// Get the time series data.
+	it := client.ListTimeSeries(ctx, req)
+	var data []monitoringpb.Point
+	for {
+		resp, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			// Handle error.
+			panic(err)
+		}
+		// Use resp.
+		for _, point := range resp.GetPoints() {
+			data = append(data, *point)
+		}
+	}
+	return data
+}
+
+func getMetricRate(ctx context.Context,
+	resourceFilter string,
+	intervalSeconds int,
+	aggregationSeconds int,
+	groupBy []string,
+	projectId string) []monitoringpb.Point {
+	client, err := monitoring.NewMetricClient(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	//Configure metric aggregation
+	aggregationStruct := &monitoringpb.Aggregation{
+		CrossSeriesReducer: monitoringpb.Aggregation_REDUCE_SUM,
+		PerSeriesAligner:   monitoringpb.Aggregation_ALIGN_RATE,
+		GroupByFields:      groupBy,
+		AlignmentPeriod: &durationpb.Duration{
+			Seconds: int64(aggregationSeconds),
+		},
+	}
+
+	//Configure metric interval
+	startTime := time.Now().UTC().Add(time.Second * -time.Duration(intervalSeconds))
+	endTime := time.Now().UTC()
+
+	interval := &monitoringpb.TimeInterval{
+		StartTime: &timestamppb.Timestamp{
+			Seconds: startTime.Unix(),
+		},
+		EndTime: &timestamppb.Timestamp{
+			Seconds: endTime.Unix(),
+		},
+	}
+
+	req := &monitoringpb.ListTimeSeriesRequest{
+		Name:        "projects/" + projectId,
+		Filter:      resourceFilter,
+		Interval:    interval,
+		Aggregation: aggregationStruct,
+	}
+
+	// Get the time series data.
+	it := client.ListTimeSeries(ctx, req)
+	var data []monitoringpb.Point
+	for {
+		resp, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			// Handle error.
+			panic(err)
+		}
+		// Use resp.
+		for _, point := range resp.GetPoints() {
+			data = append(data, *point)
+		}
+	}
+	return data
 }
 
 func getMetricMean(ctx context.Context,
@@ -184,27 +428,4 @@ func getMetricMean(ctx context.Context,
 		}
 	}
 	return data
-}
-
-func getRunService(ctx context.Context,
-	service string,
-	projectId string,
-	region string) (*runpb.Service, error) {
-	client, err := run.NewServicesClient(ctx)
-	if err != nil {
-		fmt.Printf("Error getting client:\n")
-		panic(err)
-	}
-	defer client.Close()
-
-	serviceId := "projects/" + projectId + "/locations/" + region + "/services/" + service
-
-	req := &runpb.GetServiceRequest{Name: serviceId}
-	resp, err := client.GetService(ctx, req)
-	if err != nil {
-		fmt.Printf("Error getting service: %s in %s (Project ID: %s)\n", service, region, projectId)
-		panic(err)
-	}
-	return resp, err
-
 }
