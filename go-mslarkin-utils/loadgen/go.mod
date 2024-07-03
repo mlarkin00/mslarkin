a209911fde2b509524d5cf691a1b358dae102479
Matt Larkin <matthewlarkin@google.com>
2024-07-03 09:15:47 -0700

    Adding CPU loadgen utility

--- go-mslarkin-utils/loadgen/go.mod
+++ go-mslarkin-utils/loadgen/go.mod
@@ -1,3 +1,5 @@
 module github.com/mlarkin00/mslarkin/go-mslarkin-utils/loadgen
 
 go 1.21.0
+
+require github.com/mlarkin00/mslarkin/go-mslarkin-utils/goutils v0.0.0-20240627225710-1acab1fc3d9f
--- go-mslarkin-utils/loadgen/loadgen.go
+++ go-mslarkin-utils/loadgen/loadgen.go
@@ -2,61 +2,60 @@ package loadgen
 
 import (
 	"log"
-	// "strconv"
+	"fmt"
 	"time"
-	// goutils "github.com/mlarkin00/mslarkin/go-mslarkin-utils/goutils"
+	"context"
+	"runtime"
+	"net/http"
+	"strconv"
+	goutils "github.com/mlarkin00/mslarkin/go-mslarkin-utils/goutils"
 )
 
-// type Fibonacci struct {
-//     num    float64
-//     answer float64
-// }
+func cpuLoadGen(ctx context.Context, availableCpus int, targetPct float64) {
+	log.Printf("Loading %v CPUs at %v%%\n", availableCpus, targetPct)
 
-func newFibonacci(n float64, sleepMs int, *stopChan chan bool) float64 {
-
-	var answer float64
-    c1 := make(chan float64)
-    c2 := make(chan float64)
-
-	select {
-	case <- stopChan:
-		return 0
-	default:
-		if n <= 1 {
-			answer = n
-		} else {
-			go func() {
-				answer := newFibonacci(n - 1, sleepMs, stopChan)
+	timeUnitMs := float64(100)
+	runtimeMs := timeUnitMs * targetPct
+	sleepMs := timeUnitMs - runtimeMs
+	for i := 0; i < availableCpus; i++ {
+		go func() {
+			runtime.LockOSThread()
+			for {
+				begin := time.Now()
+			PartitionLoop:
+				for {
+					select {
+					case <-ctx.Done():
+						return
+					default:
+						if time.Since(begin) > time.Duration(runtimeMs)*time.Millisecond {
+							break PartitionLoop
+						}
+					}
+				}
 				time.Sleep(time.Duration(sleepMs) * time.Millisecond)
-				c2 <- answer
-			}()
-			go func() {
-				answer := newFibonacci(n - 2, sleepMs, stopChan)
-				time.Sleep(time.Duration(sleepMs) * time.Millisecond)
-				c1 <- answer   
-			}()
+			}
 
-			answer = <-c2 + <-c1
-		}
-		close(c1)
-		close(c2)
+		}()
 	}
-
-    return answer
+	<-ctx.Done()
+	log.Println("Ending Loadgen...")
 }
 
-//1 vCPU 2GB
-// targetNum: 30, numCycles: 4 | 43s, ~60%CPU, 45%Memory
-// targetNum: 25, numCycles: 6 | 45s, ~70%CPU, 20%Memory
-// targetNum: 31, numCycles: 3 | 52s, ~83%CPU, 60%Memory
-func LoadGen(targetNum float64, numCycles int, sleepMs int, *stopChan chan bool) {
-	
-	start := time.Now()
-	log.Println("Starting: Fibonacci #:", targetNum, "| Cycles:", numCycles, "| sleepMs:", sleepMs)
-	for i:=0; i < numCycles; i++ {
-		_ = newFibonacci(targetNum, sleepMs, stopChan)
-	}
-	end := time.Now()
-	totalTime := end.Sub(start)
-	log.Printf("Complete: Fibonacci #: %v | Cycles: %v | sleepMs: %v | Time: %v", targetNum, numCycles, sleepMs, totalTime)
+func cpuLoadHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithCancel(r.Context())
+	defer cancel()
+	r = r.WithContext(ctx)
+
+	targetCpuPct, _ := strconv.ParseFloat(goutils.GetParam(r, "targetCpuPct", "40"), 64)
+	durationS, _ := strconv.Atoi(goutils.GetParam(r, "durationS", "60"))
+	configCpus, _ := strconv.Atoi(goutils.GetEnv("NUM_CPU", "1"))
+
+	loadCtx, _ := context.WithTimeout(ctx, time.Duration(durationS)*time.Second)
+
+	log.Println("Starting Request Load - CPUs:", configCpus, " Pct:", targetCpuPct, " Duration:", durationS)
+
+	go cpuLoadGen(loadCtx, configCpus, targetCpuPct)
+	// log.Println("Ending Request Load")
+	fmt.Fprintf(w, "Request Load triggered\n")
 }
