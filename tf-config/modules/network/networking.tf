8dd7c4ac54f84888bcc484b9a2171fe87ede6611
Matt Larkin <matthewlarkin@google.com>
2025-07-16 15:43:50 -0700

    .

--- tf-config/modules/network/networking.tf
+++ tf-config/modules/network/networking.tf
@@ -43,3 +43,50 @@ resource "google_compute_firewall" "ai-network-https" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["allow-https"]
 }
+
+resource "google_compute_firewall" "ai-network-icmp" {
+  name        = "ai-network-icmp-allow"
+  network     = google_compute_network.ai-network.id
+  description = "Creates firewall rule allowing ICMP on the ai-network"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "ai-network-rdp" {
+  name        = "ai-network-rdp-allow"
+  network     = google_compute_network.ai-network.id
+  description = "Creates firewall rule allowing RDP on the ai-network"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3389"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "ai-network-internal" {
+  name        = "ai-network-internal-allow"
+  network     = google_compute_network.ai-network.id
+  description = "Creates firewall rule allowing internal traffic on the ai-network"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
