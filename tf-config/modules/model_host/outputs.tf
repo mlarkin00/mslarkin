d0cb035cb5d42bd8fd7c885f0d90dd62e568a7d9
Matt Larkin <matthewlarkin@google.com>
2025-07-29 11:18:33 -0700

    .

--- tf-config/modules/model_host/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_name" {
-  description = "The name of the created VM instance."
-  value       = google_compute_instance.model_host_vm.name
-}
-
-output "instance_self_link" {
-  description = "The self_link of the created VM instance."
-  value       = google_compute_instance.model_host_vm.self_link
-}
