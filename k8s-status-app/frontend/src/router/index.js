52b652baef56831e2ade29f59b56e360b7aeec38
Matt Larkin <matthewlarkin@google.com>
2026-02-04 22:11:03 +0000

    .
    Signed-off-by: Matt Larkin <matthewlarkin@google.com>

--- k8s-status-app/frontend/src/router/index.js
+++ k8s-status-app/frontend/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/'),
   routes: [
     {
       path: '/',
